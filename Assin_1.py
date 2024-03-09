b9b5e3545cfd65d3da7954464f7baae41dd353ac
pybh <162567182+pybh@users.noreply.github.com>
2024-03-09 15:21:04 +0530

    Add files via upload

---
+++ Assin_1.py
@@ -0,0 +1,6 @@
+# positive or negative number with if condition 
+sum=int(input("Enter  Positive or Negative Number : "))
+if(sum>1):
+ print("This is Positive Number")
+else :
+ print("This is Negative Number")
